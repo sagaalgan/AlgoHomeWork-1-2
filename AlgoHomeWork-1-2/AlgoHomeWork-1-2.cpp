8dfac942cfe28824d15370bc5135f933ddfd56ce
Alexandr <sagaalgan@gmail.com>
2023-05-29 01:11:11 +0300

    initial commit

---
+++ AlgoHomeWork-1-2/AlgoHomeWork-1-2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+int calc(int arr[], int n, int x)
+{
+    int left = 0, right = n - 1;
+    int middle, count = 0;
+    while (left <= right)
+    {
+        middle = (left + right) / 2;
+        if (arr[middle] > x)
+        {
+            count += right - middle + 1;
+            right = middle - 1;
+        }
+        else (arr[middle] < x);
+        {
+            left = middle + 1;
+        }
+        
+    }
+    return count;
+}
+int main()
+{
+    setlocale (LC_ALL, "Russian");
+    int arr[] = { 14, 16, 19, 32, 32, 32, 56, 69, 72 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int x;
+    std::cout << "Введите точку отсчета: ";
+    std::cin >> x;
+    int count = calc(arr, n, x);
+    std::cout << "Количество элементов, больших " << x << ": " << count << std::endl;
+    return 0;
+}
